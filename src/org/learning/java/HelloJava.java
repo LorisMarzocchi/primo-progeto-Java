52af023ad69aaeaade650663700e768e1fceeb2a
LorisMarzocchi <ml77182a@gmail.com>
2023-10-17 12:54:57 +0200

    primo-commit-java

---
+++ src/org/learning/java/HelloJava.java
@@ -0,0 +1,10 @@
+package org.learning.java;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Ciao");
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+}
